1e2acc5945be8fae86fa69653c0b63b1fffb6dff
Gabriel de Simoes Pires <gabriel.adm2607@gmail.com>
2023-12-05 15:34:00 -0300

    criação de formulario basico dia 05/12

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function enviarFormulario() {
+    // Obtenha os valores do formulário
+    var name = document.getElementById("nome").value;
+    var email = document.getElementById("email").value;
+
+    // Construa o corpo do email
+    var corpoEmail = "Nome: " + name + "\n";
+    corpoEmail += "Email: " + email + "\n";
+
+    // Configurar e enviar o email
+    var assuntoEmail = "Novo formulário de contato";
+    var destinatarioEmail = "gabriel.adm2607@gmail.com"; // Substitua pelo seu email
+
+    var linkEmail = "mailto:" + destinatarioEmail +
+                    "?subject=" + encodeURIComponent(assuntoEmail) +
+                    "&body=" + encodeURIComponent(corpoEmail);
+
+    // Abrir o cliente de email padrão com as informações fornecidas
+    window.location.href = linkEmail;
+}
